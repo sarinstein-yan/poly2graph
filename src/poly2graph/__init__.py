c2c9fa2933e7ea7b1539c5f78f277111d7eb3532
Xianquan (Sarinstein) Yan <98195226+sarinstein-yan@users.noreply.github.com>
2025-04-11 13:48:17 +0800

    PEP8 style `__init__`

--- src/poly2graph/__init__.py
+++ src/poly2graph/__init__.py
@@ -1,27 +1,3 @@
-from .hamiltonian import (
-    shift_matrix,
-    hk2hz, hk2hz_1d,
-    hz2hk, hz2hk_1d,
-    expand_hz_as_hop_dict,
-    expand_hz_as_hop_dict_1d,
-    H_1D_batch_from_hop_dict,
-    H_1D_batch_from_hz,
-    H_1D_batch,
-)
-from .skeleton2graph import skeleton2graph, skeleton2graph_batch
-from .spectral_graph import (
-    minmax_normalize, PosGoL,
-    remove_isolates,
-    add_edges_within_threshold,
-    contract_close_nodes,
-    spectral_potential,
-)
-from .SpectralGraph import SpectralGraph
-from .util import companion_batch, kron_batch, eig_batch, eigvals_batch
-from .vis import draw_spectral_graph, mark_graph_skeleton
-
-__version__ = '0.1.0'
-
 __all__ = [
     'shift_matrix',
     'hk2hz', 'hk2hz_1d',
@@ -47,3 +23,28 @@ __all__ = [
     
     'draw_spectral_graph', 'mark_graph_skeleton'
 ]
+__version__ = '0.1.0'
+__author__ = ['Xianquan (Sarinstein) Yan', 'Hakan Akgün']
+    
+
+from poly2graph.hamiltonian import (
+    shift_matrix,
+    hk2hz, hk2hz_1d,
+    hz2hk, hz2hk_1d,
+    expand_hz_as_hop_dict,
+    expand_hz_as_hop_dict_1d,
+    H_1D_batch_from_hop_dict,
+    H_1D_batch_from_hz,
+    H_1D_batch,
+)
+from poly2graph.skeleton2graph import skeleton2graph, skeleton2graph_batch
+from poly2graph.spectral_graph import (
+    minmax_normalize, PosGoL,
+    remove_isolates,
+    add_edges_within_threshold,
+    contract_close_nodes,
+    spectral_potential,
+)
+from poly2graph.SpectralGraph import SpectralGraph
+from poly2graph.util import companion_batch, kron_batch, eig_batch, eigvals_batch
+from poly2graph.vis import draw_spectral_graph, mark_graph_skeleton
