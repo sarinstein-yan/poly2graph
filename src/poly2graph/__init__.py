56af91b8d7cc146b3760ed581215bc6b85b8c4a1
Xianquan (Sarinstein) Yan <98195226+sarinstein-yan@users.noreply.github.com>
2026-02-11 12:00:32 +0800

    iclr26 cr

--- src/poly2graph/__init__.py
+++ src/poly2graph/__init__.py
@@ -26,7 +26,7 @@ __all__ = [
     
     'draw_spectral_graph', 'mark_graph_skeleton'
 ]
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 __author__ = ['Xianquan (Sarinstein) Yan', 'Hakan Akgün']
     
 
